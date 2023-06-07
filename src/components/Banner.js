1331cfe5275adc610dbe199e39a079e215dfff7b
Nadiar Paim <nadiarpaim@gmail.com>
2023-06-07 20:36:40 +0100

    components

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+export const Banner = () => {
+  return (
+    <div className="d-flex row row-2">
+      <img className="placeholder col-6"></img>
+      <div className="d-flex">
+        <h1>Alice Smith</h1>
+        <h2>Junior Front End Developer</h2>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since
+        </p>
+      </div>
+    </div>
+  );
+};
